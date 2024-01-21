b136f96413d13c8861418a57d30ede85b2fbdc49
Ethan Gallagher <ewp.gallagher@me.com>
2024-01-21 15:47:45 +0000

    Added sorting dataset code and testing labels

--- backend/ml/train_models.py
+++ backend/ml/train_models.py
@@ -1,6 +1,30 @@
+import csv
 import keras
+import os
+import shutil
 
 DATASET_DIRECTORY = "/Users/ethan/Downloads/diabetic-retinopathy-detection/train"
+
+def sort_dataset():
+    label_dict = {}
+
+    with open('trainLabels.csv', 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            if len(row) == 2:
+                image_name, label = row
+                label_dict[image_name + '.jpeg'] = label
+
+    for label in set(label_dict.values()):
+        label_directory = os.path.join(DATASET_DIRECTORY, label)
+        os.makedirs(label_directory, exist_ok=True)
+
+    for filename in os.listdir(DATASET_DIRECTORY):
+        if filename in label_dict:
+            label = label_dict[filename]
+            source_path = os.path.join(DATASET_DIRECTORY, filename)
+            destination_path = os.path.join(DATASET_DIRECTORY, label, filename)
+            shutil.move(source_path, destination_path)
 
 def load_dataset():
     dataset = keras.utils.image_dataset_from_directory(
