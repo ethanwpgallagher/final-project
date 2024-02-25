56a53018775ee409c8e3111dadb2a23532177a77
Ethan Gallagher <ewp.gallagher@me.com>
2024-02-25 13:25:17 +0000

    Image upload and deletion working with model selection

--- client/src/pages/Diagnosis.js
+++ client/src/pages/Diagnosis.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopMenu from '../components/TopMenu';
 import DiagnosisContent from '../components/DiagnosisContent';
 import Footer from '../components/Footer';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-    root: {
+const useStyles = makeStyles(theme => (
+    {
+    root: 
+    {
       display: 'flex',
     },
   }));
 
-function Diagnosis() {
+function Diagnosis() 
+{
     const classes = useStyles();
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleFileChange = (event) => 
+    {
+      try {
+          const file = event.target.files[0];
+
+          if (file) {
+            if (file.type === 'image/jpeg' || file.type === 'image/png') {
+              setSelectedFile(file);
+              setError(null);
+            } else {
+              setSelectedFile(null);
+              setError('Invalid file type. Please select a JPEG or PNG file.');
+            }
+          }
+      } catch (error) {
+        setSelectedFile(null)
+      }
+    };
   
     return (
       <div className={classes.root}>
         <TopMenu />
-        <DiagnosisContent />
+        <DiagnosisContent
+            handleFileChange={handleFileChange}
+            selectedFile={selectedFile}
+            error={error} 
+        />
         <Footer />
       </div>
     );
